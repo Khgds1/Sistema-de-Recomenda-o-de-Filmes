3d6df747cf55653e07631c201f5f041d41c868da
khgds1 <khgds1@hotmail.com>
2020-06-05 02:49:16 -0300

    importando lista de usuarios

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ from revis import reco
 #
 reco.titulo('Seja Bem-vindo ao Revis, seu Recomedador de filmes')
 user = int(input('Qual usuário deseja acessar?: '))
+
+reco.movies(3, 1)
+reco.users(3, 1)
--- revis/reco.py
+++ revis/reco.py
@@ -1,9 +1,18 @@
 from csv import reader
 #
+with open('ratings.csv', 'r', encoding='utf8') as read_obj:
+    csvreader = reader(read_obj)
+    usuarios = list(csvreader)
+#info - 0 = userId, 1 = movieId, 2 = rating, 3 = timestamp    
+def users(ind, pos):
+    print(usuarios[ind][pos])
+#
 with open('movies.csv', 'r', encoding="utf8") as read_obj:
     csvreader = reader(read_obj)
-    list = list(csvreader)
-    print(list)
+    filmes = list(csvreader)
+#info - 0 = movieId, 1 = title, 2 = genres    
+def movies(ind, pos):
+    print(filmes[ind][pos]) 
 #
 def titulo(msg):
     print('-'*50)
