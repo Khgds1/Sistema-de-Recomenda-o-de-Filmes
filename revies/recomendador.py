580e23e5ef9a73529236cffb2428a0ee5e1a79c9
khgds1 <khgds1@hotmail.com>
2020-06-08 11:03:36 -0300

    importando base de dados MovieLens

---
+++ revies/recomendador.py
@@ -0,0 +1,17 @@
+#Importações
+import csv
+
+##Importando Ratings.csv
+rcomp = []
+with open('ratings.csv', 'r', encoding='utf8') as file:
+    ndf = csv.reader(file)
+    for linha in ndf:
+        rcomp.append(linha)
+
+##Importando Movies.csv
+mcomp = []
+with open('movies.csv', 'r', encoding='utf8') as file:
+    ndf = csv.reader(file)
+    for linha in ndf:
+        rcomp.append(linha)
+        
